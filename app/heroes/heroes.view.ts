e8f4af612039971d5169239cb29318a13edd7d7e
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-10-13 00:29:06 +0300

    update

--- app/heroes/heroes.view.ts
+++ app/heroes/heroes.view.ts
@@ -2,13 +2,18 @@ namespace $.$$ {
 	export class $story_app_heroes extends $.$story_app_heroes {
 
 
+		@$mol_mem
 		heroes() {
 			console.log( 'heroes' )
-			return this.$.$story_fetch.json( '/hero' ) as []
+			return this.$.$story_fetch.json( '/hero' ) as { name: string, '@rid': string }[]
 		}
 
 		heroes_list(): readonly any[] {
 			return this.heroes().map( hero => this.Hero( hero[ '@rid' ] ) )
+		}
+
+		hero_name( id: any ): string {
+			return this.heroes().find( hero => hero[ '@rid' ] === id )?.name ?? ''
 		}
 
 	}
