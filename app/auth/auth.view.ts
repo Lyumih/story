68b182285ce03cd254a9f501e3598b1f5955be80
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-10-20 20:39:56 +0300

    field

---
+++ app/auth/auth.view.ts
@@ -0,0 +1,13 @@
+namespace $.$$ {
+	export class $story_app_auth extends $.$story_app_auth {
+		
+		login(next?: any) {
+			console.log( 'login' )
+		}
+
+		registration( next?: any ) {
+			console.log('registration')
+		}
+		
+	}
+}
--- app/battle/field/field.view.ts
+++ app/battle/field/field.view.ts
@@ -19,5 +19,21 @@ namespace $.$$ {
 		x_list( id_y: string ): readonly any[] {
 			return [ 1, 2, 3 ].map( x => this.Cell( id_y + '_' + x ) )
 		}
+
+		cell_title( id: string ): string {
+			return id
+		}
+
+		click_cell(id?: string) {
+			console.log(this.Cell(id).attr)
+			console.log('cell', id)
+			this.cell_active(id, !this.cell_active(id))
+		}
+
+		// @ $mol_mem
+		// cell_type( id: any, next?: any ): string {
+		// 	const rand_cell = Math.random()
+		// 	return rand_cell < 0.3 ? 'hero' : rand_cell < 0.7 ? 'enemy' : ''
+		// }
 	}
 }
