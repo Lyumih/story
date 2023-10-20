188b918968426b50ee4beb0a36d1c25fb16f3794
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-10-20 21:16:17 +0300

    add group

--- app/auth/auth.view.ts
+++ app/auth/auth.view.ts
@@ -3,6 +3,7 @@ namespace $.$$ {
 		
 		login(next?: any) {
 			console.log( 'login' )
+			const result = this.$.$story_fetch.json( `/auth/login?name=${this.name()}`)
 		}
 
 		registration( next?: any ) {
